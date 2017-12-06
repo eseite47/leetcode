994b83ec447c9588576ccf3ffc68232c1e20b6d0
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-12-06 11:48:08 -0500

    Solution for Longest String

--- 3_LongestStringWithoutRepeat.js
+++ 3_LongestStringWithoutRepeat.js
@@ -3,29 +3,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  let head = 0
-  let tail = 0
-  let visitedLetters = []
-  let longestSubstring = ''
+    let head = 0
+    let tail = 0
+    let visitedLetters = []
+    let longestSubstring = ''
 
-  while (head < s.length){
-      if (visitedLetters.includes(s[tail]) || tail === s.length){
-          let currentString = s.substring(head, tail)
-          console.log('current', currentString, head, tail)
-          if (currentString.length > longestSubstring.length){
-              longestSubstring = currentString
-          }
-          head++
-          visitedLetters = [s[head]]
-          tail = head + 1
-      }
-      else {
-          console.log('s[tail]', s[tail])
-          visitedLetters.push(s[tail])
-          tail++
-      }
-  }
-  console.log(longestSubstring)
-  return longestSubstring.length
-
+    while (head < s.length){
+        if(visitedLetters.includes(s[tail]) || tail === s.length){
+            let currentString = s.substring(head, tail)
+            if(currentString.length > longestSubstring.length){
+                longestSubstring = currentString
+            }
+            head++
+            visitedLetters = [s[head]]
+            tail = head+1
+        }
+        else {
+            visitedLetters.push(s[tail])
+            tail++
+        }
+    }
+    return longestSubstring.length
 };
