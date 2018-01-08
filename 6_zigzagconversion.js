9ad31413e171d7fea81c8adfde0a825a7089d9fa
Elisabeth Seite <elisabeth.seite@gmail.com>
2018-01-08 12:35:44 -0500

    completed ZigZag Conversion

--- 6_zigzagconversion.js
+++ 6_zigzagconversion.js
@@ -1,32 +1,48 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
 var convert = function(s, numRows) {
   let convertedString = ''
   let conversionArr = []
   let counter = 0
-
+  let zigzaging = false
+  let zigCounter = 0
+  
   for (var i = 0; i <= s.length; i++){
-    conversionArr[i] = []
-    for (var j = 0; j < numRows; j++){
-      if (i % 2 === 1 && j % 2 === 1 || i % 2 === 0 || numRows === 1){
+  conversionArr[i] = []
+
+    if (!zigzaging || numRows === 1 || numRows === 2){
+      for (var j = 0; j < numRows; j++){
         conversionArr[i][j] = s[counter] || ' '
         counter++
+        zigCounter++
       }
-      else {
-      conversionArr[i][j] = ' '
-      }
-      if (counter > s.length){
-        break
+      if (zigCounter >= numRows){
+        zigzaging = !zigzaging
+        zigCounter -= 2
       }
     }
+    else if (zigzaging){
+      conversionArr[i][zigCounter] = s[counter] || ' '
+      counter++
+      zigCounter--
+      if (zigCounter <= 0){
+        zigzaging = !zigzaging
+      }
+    }
+    if (counter > s.length){
+      break
+    }
   }
-  console.log(conversionArr)
-
+  
   for (var i = 0; i < numRows; i++){
     for (var j = 0; j < conversionArr.length; j++){
-      if (conversionArr[j][i] && conversionArr[j][i] !== ' ') {convertedString += conversionArr[j][i]}
+      //console.log('i', i, 'j', j, conversionArr[j][i])
+      if(conversionArr[j][i] && conversionArr[j][i] !== ' ')
+      convertedString += conversionArr[j][i]
     }
   }
   return convertedString
 };
-
-//console.log("PAHNAPLSIIGYIR", convert("PAYPALISHIRING", 3))
-console.log('ABC', convert('ABC', 2))
