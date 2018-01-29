2cf47df51ef4d486b1ba6a17a6b8153a0332b2a0
Elisabeth Seite <elisabeth.seite@gmail.com>
2018-01-29 17:33:53 -0500

    Added solution for 9_PalindomeNumber

--- 7_ReverseInteger.js
+++ 7_ReverseInteger.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    let test = x.toString().split('').reverse()
+    if(test.length === 1){
+      return x
+    }
+    if(test[0] === '0'){
+      test.shift()
+    }
+    if(test[test.length-1] === '-'){
+      test.pop()
+      test.unshift('-')
+    }
+    let result = parseInt(test.join(''))
+    if (Math.abs(result) > Math.pow(2, 31)){
+      return 0
+    }
+    return result
 };
